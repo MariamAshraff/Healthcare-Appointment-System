57066fcafa5aa198f7d46bc0e64bc717a786a678
Mariam Ashraf <mariamashrafff163@gmail.com>
2026-04-08 23:45:59 +0200

    Add components & services

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
+import { Component } from '@angular/core';
 // app.routes.ts
 import { Routes } from '@angular/router';
 import { PatientProfile } from './features/patient/profile/patient-profile/patient-profile';
+import { Appointment } from './features/admin/appointment/appointment';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'admin', pathMatch: 'full' },
@@ -11,6 +13,10 @@ export const routes: Routes = [
    {
       path: 'patients/:id',
       component: PatientProfile
+    },
+    {
+      path:'appointments',
+      component:Appointment
     }
 
 
