2fafd0884dd2aa952b16756eaa5b48bf838d8a53
Mariam Ashraf <mariamashrafff163@gmail.com>
2026-04-09 22:43:47 +0200

    Design home page to be impressive

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { IDoctor } from '../../core/models/doctor';
+import { DoctorService } from './../../core/service/doctor-service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements OnInit {
 
+  /**
+   *
+   */
+  doctors: IDoctor[]|null=null
+  count:number=1;
+  constructor(private DoctorService : DoctorService) {
+
+
+  }
+  ngOnInit(): void {
+  this.DoctorService.getAllDoctors().subscribe({
+    next: (data) => {
+      this.doctors = data.slice(0, 7);
+    },
+    error: (err) => console.error('Error fetching appointments', err)
+  });
+}
 }
